644168c6456ca2cfe2be5f443e1dcdb39fc014dd
Edwin Cheong <edwin.cheong@revenuemonster.my>
2022-10-19 02:08:41 +0800

    image gen

---
+++ gen_image.py
@@ -0,0 +1,11 @@
+import typer
+import torch
+from matplotlib import pyplot as plt
+
+from diffusers import StableDiffusionPipeline
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    typer.run(main)
