dbfbe321b941801a37bf4dac1abd831b77561a99
Edwin Cheong <edwin.cheong@revenuemonster.my>
2022-10-29 18:09:35 +0800

    fix: Memory does not explode now

--- gen_image.py
+++ gen_image.py
@@ -1,11 +1,39 @@
 import typer
 import torch
+from torch import autocast
 from matplotlib import pyplot as plt
 
 from diffusers import StableDiffusionPipeline
 
-def main():
-    pass
+from matplotlib import pyplot as plt
+
+def main(
+    txt_input:str=typer.Option(default=''),
+    num_images:int=typer.Option(default=3),
+):
+    pipe = StableDiffusionPipeline.from_pretrained(
+        "CompVis/stable-diffusion-v1-4",
+        revision="fp16", 
+        torch_dtype=torch.float16,
+        use_auth_token=True,
+    )
+    pipe = pipe.to("cuda")
+    pipe.enable_attention_slicing()
+
+    # prompt = [txt_input] * num_images
+    prompt = txt_input
+    
+    if torch.cuda.is_available():
+        with autocast('cuda'):
+            torch.cuda.empty_cache() # Ensure app does not explode from memory
+            results = pipe(prompt, num_inference_steps=50, height=512, width=512)
+    
+    plt.figure(figsize=(15, 10))
+    for index, image in enumerate(results.images):
+        plt.subplot(2, 5, index+1)
+        plt.imshow(image)
+
+    plt.savefig('output.jpg')
 
 if __name__ == '__main__':
     typer.run(main)
